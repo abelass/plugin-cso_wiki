c18fa9753bda2557428e239bc23bd26633c71974
abelass <websolutions@mychacra.net>
2012-10-23 08:54:05 +0200

    limiter l'affichage des champs extras là ou il se doit

--- base/wiki_cso.php
+++ base/wiki_cso.php
@@ -19,15 +19,25 @@ function wiki_cso_declarer_champs_extras($champs = array()) {
 				)
   );
   
-  $c= array('extra_1','extra_2','extra_3','extra_4','extra_5','extra_6','extra_7','extra_8','extra_9','extra_10','extra_11');
+   $titre='';
+    if(_request('id_article')){
+        $id_article=_request('id_article');
+        $sql=sql_fetsel('titre,id_rubrique','spip_articles','id_article='.$id_article);
+        $titre=$sql['titre'];
+        $id_rubrique=$sql['id_rubrique'];
+        }
+    if($id_rubrique==2) $c= array('extra_1','extra_2','extra_3','extra_4','extra_5','extra_6','extra_7','extra_8','extra_9','extra_10');
+    if($id_rubrique==3) $c= array('extra_1','extra_2','extra_3','extra_4','extra_5','extra_6','extra_7','extra_8','extra_9','extra_10','extra_11');    
+    if(in_array($id_rubrique,array(2,3))){
+  
+
   foreach($c AS $champ)  {
-    $titre='';
-    if(_request('id_article'))$titre=sql_getfetsel('titre','spip_articles','id_article='._request('id_article'));
+
     $champs['spip_articles'][$champ] = array(
       'saisie' => 'textarea',//Type du champ (voir plugin Saisies)
       'options' => array(
             'nom' => $champ, 
-            'label' => _T('wiki_cso:'.$champ,array('titre'=>$titre)), 
+            'label' => _T('wiki_cso:'.$champ.'_'.$id_rubrique,array('titre'=>$titre)), 
             'sql' => "text NOT NULL DEFAULT ''",
             'versionner' => true,
             'traitements' => '_TRAITEMENT_TYPO',
@@ -39,7 +49,8 @@ function wiki_cso_declarer_champs_extras($champs = array()) {
                         ),//Seuls les webmestres peuvent modifier
                 )
         );  
-      
+       
+  }
   }
 
  
