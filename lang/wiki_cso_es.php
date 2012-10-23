31a6a5e21a27c36322cac61671c0eb3a816fa80a
abelass <websolutions@mychacra.net>
2012-10-23 10:12:31 +0200

    Les traductions

---
+++ lang/wiki_cso_es.php
@@ -0,0 +1,57 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+
+	// B
+	'best_practice' => 'Best practice',
+
+	// C
+	'case_study' => 'Case Study',
+	'choisir_lien_article' => 'Resource linked to:',
+	'cfg_exemple' => 'Example',
+	'cfg_exemple_explication' => 'Explication de cet exemple',
+	'cfg_titre_parametrages' => 'Configuration',
+	
+    //E
+    'explanation_ressource_type'=>'Here comes my explanation',   
+    'extra_1_2'=>'Normas mínimas para una sociedad civil eficaz',
+    'extra_2_2'=>'Condiciones cruciales que favorecen la eficacia del desarrollo de las OSC: Políticas y prácticas gubernamentales',
+    'extra_3_2'=>'PASO 1 CONTEXTUALIZAR LA EFICACIA DEL DESARROLLO DE LAS OSC',
+    'extra_4_2'=>'PASO 2 DEFINIR AQUELLO QUE QUIERE CAMBIAR',
+    'extra_5_2'=>'PASO 3 SABER QUIÉN PUEDE LOGRAR EL CAMBIO',
+    'extra_6_2'=>'PASO 4 ESTABLECER ALIANZAS PARA FORTALECER SU VOZ',
+    'extra_7_2'=>'PASO 5 DEFENDER SU POSTURA',
+    'extra_8_2'=>'PASO 6 TRANSMITIR SUS MENSAJES',
+    'extra_9_2'=>'PASO 7 CONSOLIDAR SU PLAN Y EVALUAR EL PROGRESO',
+    'extra_10_2'=>'Otras onformaciones',    
+    'extra_1_3'=>'Qué siginifica  @titre@?',
+    'extra_2_3'=>'¿Cómo están poniendo en práctica el @titre@ las OSC de todo el mundo?',
+    'extra_3_3'=>'Preguntas guía: ¿Cómo observo mi contexto, mi organización y a mí mismo?',
+    'extra_4_3'=>'Preguntas guía: ¿Estoy considerando todos los ángulos? ',
+    'extra_5_3'=>'Preguntas guía: ¿Qué quiero lograr?',
+    'extra_6_3'=>'Algunos objetivos propuestos para respetar y promover los derechos humanos y la justicia social',
+    'extra_7_3'=>'Preguntas guía: ¿Cómo planear, monitorear y evaluar programas y actividades?',
+    'extra_8_3'=>'Actividades para empezar a trabajar con este Principio',
+    'extra_9_3'=>'¿Cómo es el @titre@ en la práctica?',
+    'extra_10_3'=>'Voces de OSC sobre el @titre@',
+    'extra_11_3'=>'Seguir profundizando',
+
+
+	
+	// T
+	'titre_page_configurer_wiki_cso' => 'Wiki Cso-effectiveness',	
+	
+	// V
+	'voices' => 'Open Forum Voices',	
+
+	
+	// W
+	'wiki_cso_titre' => 'Wiki Cso-effectiveness',	
+);
+
+?>
--- lang/wiki_cso_fr.php
+++ lang/wiki_cso_fr.php
@@ -5,23 +5,44 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// W
-	'wiki_cso_titre' => 'Wiki Cso-effectiveness',
+
+
 
 	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
+	'choisir_lien_article' => 'Resource linked to:',
+	
+    //E
+    'explanation_ressource_type'=>'Here comes my explanation',   
+    'extra_1_2'=>'Normes minimales ou conditions préalables à une société civile efficace',
+    'extra_2_2'=>'CONDITIONS INDISPENSABLES POUR UNE CONTRIBUTION EFFICACE DES OSC AU DEVELOPPEMENT POLITIQUES ET PRATIQUES DES GOUVERNEMENTS',
+    'extra_3_2'=>'1ère ÉTAPE PLACER L’EFFICACITÉ DE LA CONTRIBUTION DES OSC AU DÉVELOPPEMENT DANS SON CONTEXTE',
+    'extra_4_2'=>'2ème ÉTAPE DÉFINIR CE QUE VOUS VOULEZ CHANGER',
+    'extra_5_2'=>'3ème ÉTAPE SAVOIR QUI PEUT FAIRE ADVENIR LE CHANGEMENT',
+    'extra_6_2'=>'4ème ÉTAPE FORGER DES ALLIANCES POUR DONNER PLUS DE POIDS À VOTRE DISCOURS',
+    'extra_7_2'=>'5ème ÉTAPE EXPOSER VOS ARGUMENTS',
+    'extra_8_2'=>' 6ème ÉTAPE FAIRE PASSER VOS MESSAGES',
+    'extra_9_2'=>'7ème ÉTAPE CONSOLIDER VOTRE PLAN ET ASSURER LE SUIVI DES PROGRÈS',
+    'extra_10_2'=>'Autres onformations',    
+    'extra_1_3'=>'Que signifie le @titre@ mean ?',
+    'extra_2_3'=>'Comment les OSC à travers le monde mettent-elles le @titre@ principe en pratique ?',
+    'extra_3_3'=>'Questions à se poser: De quelle manière prendre en compte mon contexte, mon organisation, et ma personne ?',
+    'extra_4_3'=>'Questions à se poser: Suis-je en train de considérer tous les angles possibles ?',
+    'extra_5_3'=>'Questions à se poser: Qu’est-ce que je veux accomplir ?',
+    'extra_6_3'=>'Proposition d’objectifs dans le but de respecter et promouvoir les droits de l’homme et la justice sociale',
+    'extra_7_3'=>'Questions à se poser: Comment planifier, suivre, et évaluer les programmes et les activités ? ',
+    'extra_8_3'=>'Activités pour commencer à travailler avec les Principes',
+    'extra_9_3'=>'Le @titre@ en pratique c’est quoi ?',
+    'extra_10_3'=>'Ce que disent les OSC sur le @titre@',
+    'extra_11_3'=>'Pour aller plus loin',
+
 	
 	// T
-	'pays' => 'Pays',	
 
-	//R
-	'ressource_description' => 'Ressource',
-
-	// T
-	'texte_nr_ressource' => 'Nr de ressource',
 	'titre_page_configurer_wiki_cso' => 'Wiki Cso-effectiveness',	
+
+
+	// W
+	'wiki_cso_titre' => 'Wiki Cso-effectiveness',	
 );
 
 ?>
